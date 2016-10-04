465b27876afb5ac5aa424874cc0478d4c6cfc0bb
Jorge De Corte <jorge.decorte@cozmos.be>
2016-10-04 17:45:06 +0200

    add swagger

--- app.js
+++ app.js
@@ -36,3 +36,21 @@ mobile.tables.initialize()
         app.use(mobile);    // Register the Azure Mobile Apps middleware
         app.listen(process.env.PORT || 3000);   // Listen for requests
     });
+
+    var table = azureMobileApps.table();
+
+    // Define the columns within the table
+    table.columns = {
+        "text": "string",
+        "complete": "boolean"
+    };
+    table.seed = [
+        { text: 'Example 1', complete: false },
+        { text: 'Example 2', complete: true }
+    ];
+
+    // Turn off dynamic schema
+    table.dynamicSchema = false;
+
+    // Require authentication to access the table
+    table.access = 'authenticated';
